7ee1d35e8955ed283e52ffdd18f9b5212850cbaa
PC53 <s2104454@ed.ac.uk>
2024-02-28 14:40:39 +0000

    initialise the extension

---
+++ sign-speak-chrome-extension/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Sign Speak</h1>
+      
+      <p>Sign Speak is a chrome extension that translates text to sign language.</p>
+    </div>
+  );
+}
